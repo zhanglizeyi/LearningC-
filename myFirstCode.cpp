ed0250feee288c6f3f5c9f441f3f4f0d922c81ea
zhanglizeyi <zhanglizeyi@gmail.com>
2016-04-10 00:46:35 -0700

    first push

---
+++ myFirstCode.cpp
@@ -0,0 +1,14 @@
+#include <iostream>   //preprocessor
+
+using namespace std;  //
+
+
+int main(){
+	cout << "This is my first in...";
+	cout << endl; //change lines does same as \n
+	cout << "end " << endl;
+
+	cin.get();  //enter key to pass to next line 
+
+	return 0; //terminate 
+}
